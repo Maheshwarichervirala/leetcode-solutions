0b69f102f3d49937884bc8254a0f4663293f712d
Maheshwarichervirala <maheshwarichervirala@gmail.com>
2026-05-31 11:34:31 +0530

    Time: 26 ms (81.52%), Space: 107.7 MB (16.4%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+
+        Arrays.sort(asteroids);
+
+        long m = mass;
+
+        for (int i = 0; i < asteroids.length; i++) {
+
+            if (m >= asteroids[i]) {
+                m = m + asteroids[i];
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
